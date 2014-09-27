76790eeacce50dd54f43e7c826c5440256966ea8
anushyas <anushya1995cool@gmail.com>
2014-09-27 19:26:08 +0530

    first try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Athenatry::Application.config.secret_key_base = '6a56b6a882642d24e1b2b08a3a856597daeb7d9325f89820dc9dbccacfd4da78cf21692941ae2d3fd43ec95bfa02f6d04afebcd41d5112bdb2b123327ba94d12'
